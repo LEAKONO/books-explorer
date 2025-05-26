42571cad582dca05d0367984f9189b0a30f49b84
LEAKONO <leakonoemmanuel3@gmail.com>
2025-05-26 15:05:49 +0300

    Implement responsive grid layout and styled book cards with dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Books from './components/BookCard'
 function App() {
   return (
     <div>
-    <h1 className=' font-extrabold text-center p-5 text-lg'>Books Explorer</h1>
+    <h1 className=' font-extrabold text-center p-2 text-lg'>Books Explorer</h1>
     <Books/>
     </div>
   )
--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function FilterBar({ filtered }) {
   return (
-    <div className=' flex flex-wrap'>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-3">
       {filtered.map((book) => {
         return (
           <div
             key={book.id}
-            className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center mb-4"
+            className="bg-gray-800 text-white shadow-md rounded-xl p-4 flex flex-col items-center mb-2 "
           >
             <img
               src={book.image}
@@ -15,8 +15,8 @@ function FilterBar({ filtered }) {
               className="w-24 h-32 object-cover rounded mb-2"
             />
             <h1 className='text-lg bold font-bold '>{book.title}</h1>
-            <h3 >{book.author}</h3>
-            <h3>{book.category}</h3>
+            <h3 className='font-semibold' >{book.author}</h3>
+            <h3 className='font-extralight'>{book.category}</h3>
           </div>
         );
       })}
