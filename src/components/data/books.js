cb7187b32accbd4f254d8e928ebb3399bda3d15b
LEAKONO <leakonoemmanuel3@gmail.com>
2025-05-26 12:10:53 +0300

    initial commit

---
+++ src/components/data/books.js
@@ -0,0 +1,73 @@
+// data/books.js
+export const books = [
+  {
+    id: 1,
+    title: "1984",
+    author: "George Orwell",
+    category: "Fiction",
+    image: "https://covers.openlibrary.org/b/id/7222246-L.jpg"
+  },
+  {
+    id: 2,
+    title: "Sapiens",
+    author: "Yuval Noah Harari",
+    category: "Non-Fiction",
+    image: "https://covers.openlibrary.org/b/id/10584398-L.jpg"
+  },
+  {
+    id: 3,
+    title: "Dune",
+    author: "Frank Herbert",
+    category: "Sci-Fi",
+    image: "https://covers.openlibrary.org/b/id/8101356-L.jpg"
+  },
+  {
+    id: 4,
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "Self-Help",
+    image: "https://covers.openlibrary.org/b/id/11120904-L.jpg"
+  },
+  {
+    id: 5,
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    category: "Classic",
+    image: "https://covers.openlibrary.org/b/id/8228691-L.jpg"
+  },
+  {
+    id: 6,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    category: "Fantasy",
+    image: "https://covers.openlibrary.org/b/id/6979861-L.jpg"
+  },
+  {
+    id: 7,
+    title: "Educated",
+    author: "Tara Westover",
+    category: "Memoir",
+    image: "https://covers.openlibrary.org/b/id/9251472-L.jpg"
+  },
+  {
+    id: 8,
+    title: "Brave New World",
+    author: "Aldous Huxley",
+    category: "Fiction",
+    image: "https://covers.openlibrary.org/b/id/8773271-L.jpg"
+  },
+  {
+    id: 9,
+    title: "Becoming",
+    author: "Michelle Obama",
+    category: "Memoir",
+    image: "https://covers.openlibrary.org/b/id/9259153-L.jpg"
+  },
+  {
+    id: 10,
+    title: "The Lean Startup",
+    author: "Eric Ries",
+    category: "Business",
+    image: "https://covers.openlibrary.org/b/id/10586954-L.jpg"
+  }
+];
